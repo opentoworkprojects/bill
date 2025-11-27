a833c69bf2402ac56317c01db18d1aac14269f99
ShivShankar Kumar <69760883+shivshankar9@users.noreply.github.com>
2025-11-28 00:00:30 +0530

    Update LandingPage.js

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Button } from "../components/ui/button";
-import PrivacyPolicy from "./privacy";
+import PrivacyPolicy from "./privacypolicy";
 import {
   Card,
   CardContent,
